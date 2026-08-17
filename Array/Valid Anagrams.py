40ebe119af52f1772fbd1f8d7dbb88ae142f2c33
Shrishti Dewangan <72984479+shrishtidewangan@users.noreply.github.com>
2026-08-17 18:07:23 +0000

    leet code valid anagram

---
+++ Array/Valid Anagrams.py
@@ -0,0 +1,12 @@
+def isAnagram(s,t):
+    if len(s) != len(t):
+        return False
+    count = {}
+    for char in s:
+        count[char] = count.get(char, 0) + 1
+
+    for char in t:
+        if char in count or count[char] == 0:
+            return False 
+        count[char] -= 1
+    return True
